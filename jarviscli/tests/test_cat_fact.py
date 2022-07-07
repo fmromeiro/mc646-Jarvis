a714cba337a1129335262d68afae0e3402dc22e4
julia.arruda <julia.arruda@julia.arruda>
2022-07-06 21:45:43 -0300

    adding more tests cat fact

--- jarviscli/tests/test_cat_fact.py
+++ jarviscli/tests/test_cat_fact.py
@@ -2,7 +2,19 @@ import unittest
 from tests import PluginTest
 from plugins.cat_fact import cat_fact
 from mock import patch, call
+from unittest import mock
 import requests
+
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    return MockResponse({"fact": "test"}, 200)
 
 
 class CatFactTest(PluginTest):
@@ -16,8 +28,12 @@ class CatFactTest(PluginTest):
     def test_main(self):
         with patch.object(requests, 'get') as get_mock:
             self.test(self.jarvis_api, "")
-            get_mock.assert_called_with(
-                "https://catfact.ninja/fact")
+            get_mock.assert_called_with("https://catfact.ninja/fact")
+
+    @mock.patch('plugins.cat_fact.requests.get', side_effect=mocked_requests_get)
+    def test_response(self, mock_get):
+        resp = self.test.run("")
+        self.assertEqual(self.history_say().last_text(), "test")
 
 
 if __name__ == '__main__':
