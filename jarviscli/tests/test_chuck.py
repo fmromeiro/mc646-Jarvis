c01757160f987370c7c3c7e18efc79905ec6a8a0
Lucas Dinnouti <lucas.dinnouti@wavy.global>
2022-07-06 21:50:23 -0300

    Add chuck tests

--- jarviscli/tests/test_chuck.py
+++ jarviscli/tests/test_chuck.py
@@ -4,6 +4,16 @@ from plugins.chuck import chuck
 from mock import patch, call
 import requests
 
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    return MockResponse({"value": "test"}, 200)
 
 class ChuckTest(PluginTest):
     """
@@ -19,6 +29,10 @@ class ChuckTest(PluginTest):
             get_mock.assert_called_with(
                 "https://api.chucknorris.io/jokes/random")
 
-
+    @patch('plugins.chuck.requests.get', side_effect=mocked_requests_get)
+    def test_response(self, mock_get):
+        resp = self.test.run("")
+        self.assertEqual(self.history_say().last_text(), "test")
+ 
 if __name__ == '__main__':
     unittest.main()
